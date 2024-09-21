f37e003eaf6e5f6f77cc281902e1136d68130d7c
Shiloh Lee <as97108@gmail.com>
2024-09-21 15:10:34 +0800

    update : Add 2 appraoches for sum 1 to n Create 2 funtion to solve sum 1 to N using recursive and trapezoidal area formula

--- week-02/sum.js
+++ week-02/sum.js
@@ -29,6 +29,25 @@ function sumRecursive(ary){
     return lastValue + sumRecursive(ary);
 }
 
+// 如果input為n要return 1+2+...+n 不用for while
+
+// approach 1: recurisive
+
+function sum1toNRecursive(n){
+    return n == 1 ?  1 : n + sum1toNRecursive(n - 1) ;
+}
+
+// approach 2: 上底加下底
+
+function sum1toN(n){
+    return (1 + n) * n / 2;
+}
+
 console.log(sum([1, 5, 3, 2])); // 11
 console.log(sumES6([1, 5, 3, 2])); // 11
 console.log(sumRecursive([1, 5, 3, 2])); //11
+
+console.log(sum1toNRecursive(10)); // 55
+console.log(sum1toNRecursive(9));  // 45
+console.log(sum1toN(10)); // 55
+console.log(sum1toN(9)); // 45
